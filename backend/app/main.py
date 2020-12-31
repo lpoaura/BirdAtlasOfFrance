e242444c7e93d1f3b4183d66a85bd31a2948c5e2
lpofredc <frederic.cloitre@lpo.fr>
2020-12-31 14:43:42 +0100

    configure backend logs

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,43 +1,47 @@
 from typing import Any, List
 import uvicorn
-import coloredlogs, logging
+from .utils import log
 
+from fastapi.logger import logger
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
 from pydantic import UUID4
 from sqlalchemy.orm import Session
 from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND
 
-from .core.db import SessionLocal, engine
+from app.core.db import SessionLocal, engine
 
-from .core.ref_geo.routers import router as ref_geo_router
+from app.core.ref_geo.routers import router as ref_geo_router
 
-# Create all tables in the database.
-# Comment this out if you using migrations.
-# models.Base.metadata.create_all(bind=engine)
-
-logger = logging.getLogger(__name__)
-coloredlogs.install(level='DEBUG', logger=logger)
-logger.debug("this is a debugging message")
-logger.info("this is an informational message")
-logger.warning("this is a warning message")
-logger.error("this is an error message")
-logger.critical("this is a critical message")
-
-app = FastAPI()
+app = FastAPI(title='BirdAtlasOfFrance API')
+logger = log.setup_logger_from_settings()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Atlas bird of France root API"}
+    logger.debug("Hello!")
+    # return {"message": "Welcome to Atlas bird of France API"}
+    return RedirectResponse('/docs')
+
+@app.get("/pong")
+async def pong():
+    logger.error("Error log")
+    logger.warning("Warning log")
+    logger.info("Info log")
+    logger.debug("Debug log")
+    return {"ping": "pong"}
 
 app.include_router(ref_geo_router)
 
 
-
-
 def main():
-    logger.info("starting ODF app")
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True, workers=2)
+    try:
+        logger.info("starting ODF app")
+        uvicorn.run("app.main:app", host="0.0.0.0", log_level="debug", port=8888, reload=True, workers=2)
+    except Exception as e:
+        logger.critical(f"Can't start app")
+        raise e
+
 
 if __name__ == "__main__":
     main()
