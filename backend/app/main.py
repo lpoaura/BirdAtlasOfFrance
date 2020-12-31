0028d5c86fba550439d870b9d6f230cd3691c79d
lpofredc <frederic.cloitre@lpo.fr>
2020-12-31 11:35:03 +0100

    Update config

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,13 @@
 from typing import Any, List
+import uvicorn
+import coloredlogs, logging
 
 from fastapi import Depends, FastAPI, HTTPException
 from pydantic import UUID4
 from sqlalchemy.orm import Session
 from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND
 
-from app.core.db import SessionLocal, engine
+from .core.db import SessionLocal, engine
 
 from .core.ref_geo.routers import router as ref_geo_router
 
@@ -13,17 +15,29 @@ from .core.ref_geo.routers import router as ref_geo_router
 # Comment this out if you using migrations.
 # models.Base.metadata.create_all(bind=engine)
 
+logger = logging.getLogger(__name__)
+coloredlogs.install(level='DEBUG', logger=logger)
+logger.debug("this is a debugging message")
+logger.info("this is an informational message")
+logger.warning("this is a warning message")
+logger.error("this is an error message")
+logger.critical("this is a critical message")
+
 app = FastAPI()
 
 
 @app.get("/")
-def index():
-    return {"message": "Hello world!"}
-
+async def root():
+    return {"message": "Atlas bird of France root API"}
 
 app.include_router(ref_geo_router)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello Bigger Applications!"}
+
+
+def main():
+    logger.info("starting ODF app")
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True, workers=2)
+
+if __name__ == "__main__":
+    main()
