0f060124ed3106cce066ba104a08e22b6fb0964d
lpofredc <frederic.cloitre@lpo.fr>
2021-01-01 16:04:11 +0100

    Improve settings

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,11 +4,28 @@ from pydantic import BaseSettings, PostgresDsn, validator
 
 
 class Settings(BaseSettings):
-    POSTGRES_HOST: str
-    POSTGRES_PORT: str
+    """App settings from env.
+
+    Args:
+        POSTGRES_HOST (Optional, str): DB Host. Default to "localhost".
+        POSTGRES_PORT (Optional, int): DB Port. Default to 5432.
+        POSTGRES_USER (str): DB username.
+        POSTGRES_PASSWORD (str): DB password.
+        POSTGRES_DB (str): DB name.
+        LOG_LEVEL (Optional, str) : Log level. Default to "INFO"
+
+    Returns:
+        [type]: [description]
+    """
+
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
+    POSTGRES_HOST: Optional[str] = "localhost"
+    POSTGRES_PORT: Optional[str] = 5432
+    LOG_LEVEL: Optional[str] = "INFO"
+    APP_NAME: Optional[str] = "Bird atlas of France"
+    APP_SYSNAME: Optional[str] = "odf_api"
 
     SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
 
@@ -22,7 +39,8 @@ class Settings(BaseSettings):
             password=values.get("POSTGRES_PASSWORD"),
             host=values.get("POSTGRES_HOST"),
             port=values.get("POSTGRES_PORT"),
-            path=f"/{values.get('POSTGRES_DB') or  ''}?application_name=odf_api",
+            # path=f"/{values.get('POSTGRES_DB') or  ''}?application_name=odf_api",
+            path=f"/{values.get('POSTGRES_DB') or  ''}",
         )
 
     class Config:
