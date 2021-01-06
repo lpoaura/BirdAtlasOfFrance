19cfbd84c7ff2a5a1a920cbdd48a59735638891f
lpofredc <frederic.cloitre@lpo.fr>
2021-01-07 00:24:15 +0100

    add run_front script

--- run_dev.sh
+++ run_dev.sh
@@ -1,24 +1,66 @@
 #!/bin/bash
 
+# install_back() {
+#     echo "****************************************"
+#     echo "*         Installation du backend       "
+#     echo "****************************************"
+#     echo ""
+#     echo "Téléchargement et installation de PYENV"
+#     #curl https://pyenv.run | bash
+#     #git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+#     #exec $SHELL
+#     echo "Create project virtualenv (odf)"
+#     cd backend
+#     pyenv install 3.7.9
+#     pyenv virtualenv 3.7.9 odf
+#     pyenv activate odf
+#     echo ""
+#     echo "Téléchargement et installation de POETRY"
+#     echo ""
+#     curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+#     echo "Installation des dépendances du backend"
+#     cd backend
+#     poetry install
+    
+# }
+
+# install_front() {
+#     echo "Téléchargement et installation de NVM"
+#     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+#     echo "Installation des dépendances du frontend"
+#     cd frontend
+#     nvm install
+# }
+
+
+
 dev_back() {
     cd backend
     poetry run dev_back
 }
 
 dev_front() {
-    echo "Not yet implemented"
-    # cd frontend
-    # nvm use 
-    # npm run dev
+
+    cd frontend
+    nvm use
+    npm run dev
 }
 
 for value in $*
 do
-    if [ $value == "back" ]; then
+    # if [ $value == "install_back" ]; then
+    #     echo "Starting backend install"
+    #     install_back
+    # fi
+    #     if [ $value == "install_front" ]; then
+    #     echo "Starting frontend install"
+    #     install_front
+    # fi
+    if [ $value == "run_back" ]; then
         echo "Starting backend in dev mode"
         dev_back
     fi
-    if [ $value == "front" ]; then
+    if [ $value == "run_front" ]; then
         echo "Starting frontend in dev mode"
         dev_front
     fi
