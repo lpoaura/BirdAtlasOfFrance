7820797029ffb90d6402ca25d006e291db3aab49
eguilley <elsa.guilley@lpo.fr>
2021-01-12 18:29:19 +0100

    feat: [Prospecting map] add dynamic geojson

--- frontend/pages/prospecting/index.vue
+++ frontend/pages/prospecting/index.vue
@@ -1,14 +1,11 @@
 <template>
   <div>
     <h1>Page de prospection</h1>
-    <span> Envelope : {{ envelope }} </span><br />
-    <span>
-      ----------------------------------------------------------------------------------
-    </span>
-    <prospecting-map
+    <!-- <prospecting-map
       :areas-data="areasData"
       @envelopeUpdated="updateEnvelope"
-    />
+    /> -->
+    <prospecting-map />
   </div>
 </template>
 
@@ -19,20 +16,20 @@ export default {
   components: {
     'prospecting-map': ProspectingMap,
   },
-  async asyncData({ $axios }) {
-    const areasData = await $axios.$get(
-      `http://127.0.0.1:8888/api/v1/area_knowledge_level/M10?limit=50`
-      // `http://127.0.0.1:8888/api/v1/area_knowledge_level/M10?limit=50&envelope=${}`
-    )
-    return { areasData }
-  },
-  data: () => ({
-    envelope: null,
-  }),
-  methods: {
-    updateEnvelope(envelope) {
-      this.envelope = envelope
-    },
-  },
+  // async asyncData({ $axios }) {
+  //   const areasData = await $axios.$get(
+  //     `http://127.0.0.1:8888/api/v1/area_knowledge_level/M10?limit=50`
+  //     // `http://127.0.0.1:8888/api/v1/area_knowledge_level/M10?limit=50&envelope=${}`
+  //   )
+  //   return { areasData }
+  // },
+  // data: () => ({
+  //   envelope: null,
+  // }),
+  // methods: {
+  //   updateEnvelope(envelope) {
+  //     this.envelope = envelope
+  //   },
+  // },
 }
 </script>
