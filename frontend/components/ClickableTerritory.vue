c16860a8c7834b91978815b2fe6df7042cb70fcc
eguilley <elsa.guilley@lpo.fr>
2021-01-14 21:46:51 +0100

    feat: [Prospecting map] zoom to territory + seasons list

---
+++ frontend/components/ClickableTerritory.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-card @click="selectTerritory">
+      <v-card-text>
+        <span>{{ territoryData.name }}</span>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    territoryData: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    selectTerritory() {
+      this.$emit('selectedTerritory', this.territoryData.bounds)
+    },
+  },
+}
+</script>
