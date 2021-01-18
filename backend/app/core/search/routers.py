935de2b715f75d66b05d5cb60c58423d6b3c819c
lpofredc <frederic.cloitre@lpo.fr>
2021-01-18 17:25:10 +0100

    Fix: Fix #7

--- backend/app/core/search/routers.py
+++ backend/app/core/search/routers.py
@@ -9,22 +9,30 @@ from sqlalchemy.orm import Session
 
 from app.utils.db import get_db
 
-from .actions import mv_autocomplete_search
-from .schemas import MvAutocompleteSearchSchema
+from .actions import mv_search_areas, mv_search_taxa
+from .schemas import MvSearchAreasSchema, MvSearchTaxaSchema
 
 logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
 
-@router.get("/search", response_model=List[MvAutocompleteSearchSchema], tags=["core"])
-def search(
+@router.get("/search_areas", response_model=List[MvSearchAreasSchema], tags=["core"])
+def search_areas(
     db: Session = Depends(get_db),
     limit: int = 10,
     search: Optional[str] = None,
     type_code: Optional[str] = None,
 ) -> Any:
-    rsearch = mv_autocomplete_search.get_search_list(
+    rsearch = mv_search_areas.get_search_list(
         db=db, limit=limit, search=search, type_code=type_code
     )
+    return rsearch
+
+
+@router.get("/search_taxa", response_model=List[MvSearchTaxaSchema], tags=["core"])
+def search_taxa(
+    db: Session = Depends(get_db), limit: int = 10, search: Optional[str] = None
+) -> Any:
+    rsearch = mv_search_taxa.get_search_list(db=db, limit=limit, search=search)
     return rsearch
