a765413530524c97b5783554d6daef86a46400cc
lpofredc <frederic.cloitre@lpo.fr>
2021-03-03 11:35:34 +0100

    fix: filter general stats  on rank specie

--- data/sql_scripts/04_general_stats.sql
+++ data/sql_scripts/04_general_stats.sql
@@ -1,28 +1,26 @@
 /*
-TITLE
+Home - General stats
 -----
-Description
-*/
-
-DO
-$$
-    BEGIN
-        /* Vue matérialisée finale */
-        DROP MATERIALIZED VIEW IF EXISTS atlas.mv_general_stats;
-        CREATE MATERIALIZED VIEW atlas.mv_general_stats AS
-        SELECT
-                    count(DISTINCT cd_nom) FILTER (WHERE mv_data_for_atlas.new_data_all_period) AS count_taxa_all_period
-          ,         count(DISTINCT cd_nom) FILTER (WHERE mv_data_for_atlas.new_data_breeding)   AS count_taxa_breeding
-          ,         count(DISTINCT cd_nom)
-                    FILTER (WHERE mv_data_for_atlas.new_data_wintering)                         AS count_taxa_wintering
-          ,         '9999'                                                                      AS prospecting_hours_all_period
-          ,         '7777'                                                                      AS prospecting_hours_breeding
-          ,         '2222'                                                                      AS prospecting_hours_wintering
-            FROM
-                atlas.mv_data_for_atlas;
-
-        COMMIT;
-    END
-$$
-;
+Statistiques générales de la plateforme 
+ */
+DO $$
+BEGIN
+    /* Vue matérialisée finale */
+    DROP MATERIALIZED VIEW IF EXISTS atlas.mv_general_stats;
+    CREATE MATERIALIZED VIEW atlas.mv_general_stats AS
+    SELECT
+        count(DISTINCT mv_data_for_atlas.cd_nom) FILTER (WHERE mv_data_for_atlas.new_data_all_period) AS count_taxa_all_period,
+        count(DISTINCT mv_data_for_atlas.cd_nom) FILTER (WHERE mv_data_for_atlas.new_data_breeding) AS count_taxa_breeding,
+        count(DISTINCT mv_data_for_atlas.cd_nom) FILTER (WHERE mv_data_for_atlas.new_data_wintering) AS count_taxa_wintering,
+        '9999' AS prospecting_hours_all_period,
+        '7777' AS prospecting_hours_breeding,
+        '2222' AS prospecting_hours_wintering
+    FROM
+        atlas.mv_data_for_atlas
+        JOIN taxonomie.taxref ON mv_data_for_atlas.cd_nom = taxref.cd_nom
+    WHERE
+        id_rang = 'ES';
+    COMMIT;
+END
+$$;
 
