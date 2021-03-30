893c2a80bdb0e420c6d89a761d6531c608f4f013
lpofredc <frederic.cloitre@lpo.fr>
2021-03-30 14:19:33 +0200

    feat: taxa distribution API

---
+++ backend/app/core/taxa/schemas.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from geojson_pydantic.features import Feature, FeatureCollection, Geometry
+from pydantic import BaseModel
+
+
+class TaxaDistributionProperties(BaseModel):
+    status: str
+
+
+class TaxaDistributionFeature(Feature):
+    properties: TaxaDistributionProperties
+
+
+class TaxaDistributionFeaturesCollection(FeatureCollection):
+    features: List[TaxaDistributionFeature]
+
+    class Config:
+        orm_mode = True
