b8183096a0c70afae7db76bebd51b00211948595
eguilley <elsa.guilley@lpo.fr>
2021-04-20 14:45:12 +0200

    feat: Maintenance page for small devices

---
+++ frontend/pages/mobile.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container fluid>
+    <main>
+      <img class="Logo" src="/ODF-logo-filled.svg" />
+      <h1 class="Message">
+        L'Atlas des Oiseaux de France n'est
+        <b>pas encore disponible sur cette taille d'écran</b>, veuillez nous en
+        excuser.
+      </h1>
+    </main>
+  </v-container>
+</template>
+
+<style scoped>
+main {
+  padding: 16px 2%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.Logo {
+  height: 100px;
+  margin-bottom: 16px;
+}
+
+.Message {
+  font-family: 'Poppins', sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 21px;
+  color: #262626;
+  text-align: center;
+}
+</style>
