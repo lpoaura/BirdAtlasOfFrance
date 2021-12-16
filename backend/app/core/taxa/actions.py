e60b84982330fb6b1aaa5b164dd07b0671a1d96a
lpofredc <frederic.cloitre@lpo.fr>
2021-12-16 15:44:58 +0100

    fix: disable temporary WIP modules

--- backend/app/core/taxa/actions.py
+++ backend/app/core/taxa/actions.py
@@ -10,7 +10,7 @@ from app.core.commons.models import AreaKnowledgeTaxaList
 from app.core.ref_geo.models import LAreas
 from app.utils.db import Base
 
-from .models import MvTaxaAltitudeDistribution
+# from .models import MvTaxaAltitudeDistribution
 
 logger = logging.getLogger(__name__)
 
@@ -87,27 +87,27 @@ class TaxaDistributionActions(BaseReadOnlyActions[AreaKnowledgeTaxaList]):
         return q.all()
 
 
-class TaxaAltitudeDistributionActions(BaseReadOnlyActions[MvTaxaAltitudeDistribution]):
-    """[summary]
+# class TaxaAltitudeDistributionActions(BaseReadOnlyActions[MvTaxaAltitudeDistribution]):
+#     """[summary]
 
-    Args:
-        BaseReadOnlyActions ([type]): [description]
-    """
+#     Args:
+#         BaseReadOnlyActions ([type]): [description]
+#     """
 
-    def taxa_alti_distribution(self, db: Session, id_area: int, cd_nom: int = None):
+#     def taxa_alti_distribution(self, db: Session, id_area: int, cd_nom: int = None):
 
-        q = (
-            db.query(
-                MvTaxaAltitudeDistribution.range,
-                func.sum(MvTaxaAltitudeDistribution.count).label("count"),
-            )
-            .filter(MvTaxaAltitudeDistribution.id_area == id_area)
-            .group_by(MvTaxaAltitudeDistribution.range)
-            .order_by(MvTaxaAltitudeDistribution.range)
-        )
-        q = q.filter(MvTaxaAltitudeDistribution.cd_nom == cd_nom) if cd_nom is not None else q
-        return q.all()
+#         q = (
+#             db.query(
+#                 MvTaxaAltitudeDistribution.range,
+#                 func.sum(MvTaxaAltitudeDistribution.count).label("count"),
+#             )
+#             .filter(MvTaxaAltitudeDistribution.id_area == id_area)
+#             .group_by(MvTaxaAltitudeDistribution.range)
+#             .order_by(MvTaxaAltitudeDistribution.range)
+#         )
+#         q = q.filter(MvTaxaAltitudeDistribution.cd_nom == cd_nom) if cd_nom is not None else q
+#         return q.all()
 
 
 taxa_distrib = TaxaDistributionActions(AreaKnowledgeTaxaList)
-altitude_distrib = TaxaAltitudeDistributionActions(MvTaxaAltitudeDistribution)
+# altitude_distrib = TaxaAltitudeDistributionActions(MvTaxaAltitudeDistribution)
