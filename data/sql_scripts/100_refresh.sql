9868e6c736b04bb08293bd6998dc953ae2a03757
lpofredc <frederic.cloitre@lpo.fr>
2021-12-22 15:11:56 +0100

    fix: missing refresh mv

--- data/sql_scripts/100_refresh.sql
+++ data/sql_scripts/100_refresh.sql
@@ -45,27 +45,10 @@ BEGIN
     RAISE INFO '-- % -- INTERVAL FROM START SCRIPT', delta;
     RAISE INFO '-- % -- END SCRIPT', clock_timestamp();
     REFRESH MATERIALIZED VIEW atlas.mv_taxon_count_classes_by_territory;
+    REFRESH MATERIALIZED VIEW CONCURRENTLY atlas.mv_realized_epoc;
 END
 $$ LANGUAGE plpgsql
 ;
 
-SELECT atlas.refresh_materialized_view_data()
-;
 
-SELECT
-    sy.id_synthese
-  , tso.name_source
-  , sy.entity_source_pk_value
-  , dfa.*
-    FROM
-        atlas.mv_data_for_atlas dfa
-            JOIN ref_geo.l_areas la ON dfa.id_area = la.id_area
-            JOIN gn_synthese.synthese sy ON dfa.id_data = sy.id_synthese
-            JOIN gn_synthese.t_sources tso ON sy.id_source = tso.id_source
-    WHERE
---           la.area_name IN ('10kmL93E043N692')
---       AND dfa.cd_nom = 3511
---       AND dfa.new_data_all_period
-entity_source_pk_value = '104839501'
-;
 
