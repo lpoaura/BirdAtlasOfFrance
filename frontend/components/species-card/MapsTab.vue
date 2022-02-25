8935fdd93c00057d9e9eb6b06e025c5cf0b6e433
eguilley <elsa.guilley@lpo.fr>
2022-02-25 14:18:59 +0100

    feat: [Species card] map initiation

---
+++ frontend/components/species-card/MapsTab.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="SpeciesCardContent map" :class="tabStatus">
+    <client-only>
+      <lazy-species-map :selected-territory="selectedTerritory" />
+    </client-only>
+    <div class="MapLegend"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    'lazy-species-map': () => {
+      if (process.client) {
+        return import('~/components/species-card/SpeciesMap.vue')
+      }
+    },
+  },
+  props: {
+    tabStatus: {
+      type: String,
+      required: true,
+    },
+    selectedTerritory: {
+      // Territoire cliqué (FrMet ou DOM-TOM)
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.MapLegend {
+  border-left: 1px solid rgba(57, 118, 90, 0.1);
+  width: 200px;
+}
+</style>
