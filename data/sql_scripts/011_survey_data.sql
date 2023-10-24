f18d889e84e48f50c54f6e6b6d92d7a4f583114a
Fred C <frederic.cloitre@lpo.fr>
2023-10-24 16:04:32 +0200

    Graphique de tendance hivernale

--- data/sql_scripts/011_survey_data.sql
+++ data/sql_scripts/011_survey_data.sql
@@ -12,18 +12,20 @@ $$
     BEGIN
         DROP MATERIALIZED VIEW IF EXISTS atlas.mv_survey_map_data;
         CREATE MATERIALIZED VIEW atlas.mv_survey_map_data AS
-        SELECT row_number() OVER ()                                                                                                                     AS id,
+        SELECT ROW_NUMBER() OVER ()                                                                                                                     AS id,
                id_area_atlas_territory,
                id_area,
                cd_nom,
                phenologie                                                                                                                               AS phenology_period,
-               array_agg(ARRAY [unite, coalesce(value::VARCHAR, val_min::VARCHAR || '-' || val_max::VARCHAR), (extract(YEAR FROM last_date))::VARCHAR]) AS data
+               ARRAY_AGG(ARRAY [unite, COALESCE(value::VARCHAR, val_min::VARCHAR || '-' || val_max::VARCHAR), (EXTRACT(YEAR FROM last_date))::VARCHAR]) AS data
         FROM src_survey.vm_carto_reg_information
         GROUP BY id_area_atlas_territory,
                  id_area,
                  cd_nom,
                  phenologie
         ORDER BY 1, 2, 3, 4;
+
+        SELECT * FROM atlas.mv_survey_map_data;
 
 
         CREATE INDEX ON atlas.mv_survey_map_data (id_area_atlas_territory);
@@ -34,13 +36,13 @@ $$
         DROP MATERIALIZED VIEW IF EXISTS atlas.mv_survey_chart_data;
         CREATE MATERIALIZED VIEW atlas.mv_survey_chart_data AS
         (
-        SELECT row_number() OVER ()          AS id,
+        SELECT ROW_NUMBER() OVER ()          AS id,
                id_area_atlas_territory,
-               extract(YEAR FROM date_debut) AS year,
+               EXTRACT(YEAR FROM date_debut) AS year,
                cd_nom,
                phenologie                    AS phenology_period,
                unite                         AS unit,
-               jsonb_build_object('val', val, 'val_min', val_min, 'val_max',
+               JSONB_BUILD_OBJECT('val', val, 'val_min', val_min, 'val_max',
                                   val_max)   AS data
         FROM src_survey.vm_graph_information
 --         GROUP BY 2, 3, 4, 5, 6
@@ -55,16 +57,4 @@ $$
 $$
 ;
 
-
-ROLLBACK;
-BEGIN;
-INSERT INTO gn_synthese.cor_area_synthese(id_area, id_synthese)
-    (SELECT id_area, id_synthese
-     FROM gn_synthese.synthese
-              JOIN ref_geo.l_areas ON st_intersects(synthese.the_geom_local, l_areas.geom)
-     WHERE area_code ILIKE '5kmUTM21%')
-ON CONFLICT DO NOTHING;
-
-
-COMMIT;
 
