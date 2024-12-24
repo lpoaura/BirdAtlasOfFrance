4de8045530480915f6ef7996ecfc3c74bef25d46
OiseauxdeFrance <108476175+OiseauxdeFrance@users.noreply.github.com>
2024-12-24 10:53:55 +0100

    update glossary.vue

--- frontend/pages/about/glossary.vue
+++ frontend/pages/about/glossary.vue
@@ -23,6 +23,20 @@
       <span class="black02 bottom-margin-24">
         Carré de 10km de côté dans lequel s’effectue une prospection.
       </span>
+      <h4 class="black02 fw-600 bottom-margin-8">Phénologie</h4> 
+            <span class="black02 bottom-margin-24">
+      Science qui étudie les phénomènes qui marquent la vie des animaux et des plantes. Chez les oiseaux migrateurs on s’intéresse plus particulièrement aux périodes d’arrivée ou de départ en migration.
+      </span>
+      <h4 class="black02 fw-600 bottom-margin-8">Stratégie d’échantillonnage </h4>
+      <span class="black02 bottom-margin-24">
+      Comme il est impossible de recenser une population d’oiseaux entière sur le territoire, il est donc nécessaire de réaliser un échantillon sur une partie de la population qui sera analysé statistiquement pour produire des connaissances. En fonction du type de connaissances souhaitées (répartition, taille de population, phénologie*, tendances démographique), on définit une stratégie d’échantillonnage en établissant un plan d’échantillonnage qui va conditionner la façon dont on va collecter les données sur le terrain (protocole). 
+      <a href="http://ct72.espaces-naturels.fr/definir-les-modalites-de-l%E2%80%99echantillonnage" target="_blank" title="Plus d’informations">Plus d’informations</a>
+      </span>
+      <h4 class="black02 fw-600 bottom-margin-8">Variable essentielle de biodiversité (EBV)</h4>
+      <span class="black02 bottom-margin-24">
+      Les variables essentielles de biodiversité correspondent à l’ensemble des paramètres que nous devrions idéalement pouvoir renseigner pour disposer d’une représentation précise de la biodiversité, afin d’en mesurer les évolutions au fil du temps. 
+      <a href="https://naturefrance.fr/la-production-de-connaissances" target="_blank" title="Plus d’informations">Plus d’informations</a>
+      </span>   
     </section>
   </v-container>
 </template>
