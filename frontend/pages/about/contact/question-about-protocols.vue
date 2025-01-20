8a4e4db556fdeed60d052da7233e44b6970eec26
Marine Teneur <115530181+marineteneur@users.noreply.github.com>
2025-01-20 15:41:59 +0100

    Update question-about-protocols.vue MAJ Titre Prospections Atlas

--- frontend/pages/about/contact/question-about-protocols.vue
+++ frontend/pages/about/contact/question-about-protocols.vue
@@ -158,7 +158,7 @@ export default {
       'Observatoire Rapaces',
       'LIMAT',
       'Wetlands',
-      'Listes complètes et données ponctuelles',
+      'Prospections Atlas',
     ],
     emailConfigs: [
       'oiseauxdefrance',
